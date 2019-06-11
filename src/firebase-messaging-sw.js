4d5635c84bfb2843432a61f8627b43dd69cbd4f8
BenassiJosef <35779314+BenassiJosef@users.noreply.github.com>
2019-06-11 11:57:13 +0100

    Update firebase-messaging-sw.js

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-messaging.js');
 
 firebase.initializeApp({
-  'messagingSenderId': "123545357008"
+  'messagingSenderId': ""
 });
 
 const messaging = firebase.messaging();
